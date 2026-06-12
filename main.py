719a0cb828da7401f6ab3d3da869383bbc904c39
James <66457860+TristanofJugdral@users.noreply.github.com>
2026-06-12 01:11:23 -0400

    Refactor comments and add version info in main.py
    Updated comments for clarity and added versioning information.

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
-# MAIN - MC2JTD
-# Creator: @TristanofJugdral
+# main.py
+# MAIN of MC2JTD
+
+# Creator: @TristanofJugdral (Ver. 1.1.6 | 10 June 2026)
 
 import discord
 from discord.ext import commands
 import asyncio
-import os
+import time
 
-TOKEN = input("Enter your Discord bot token: ").strip()
+TOKEN = input("Enter your Discord bot token: ").strip() # Type bot's token
 
 intents = discord.Intents.default()
-intents.message_content = True # read chat
+intents.message_content = True  # Yes, the bot reads chat
 bot = commands.Bot(
-    command_prefix="mc2jtd.", # prefix: mc2jtd.
+    command_prefix="mc2jtd.",   # Prefix: mc2jtd.
     intents=intents,
     help_command=None
 ) 
 
-
 # Load cogs
 async def load_cogs():
     
-    # Receive and send messages from server
-    await bot.load_extension("cogs.msg_receive")
+    await bot.load_extension("cogs.msg_receive")    # Message receiver
     print("Loaded cogs/msg_receive.py")
     
-    # Basic commands
-    await bot.load_extension("cogs.basic_commands")
+    await bot.load_extension("cogs.basic_commands") # Commands
     print("Loaded cogs/basic_commands.py")
+    
+    await bot.load_extension("cogs.sftp_reader")    # SFTP
+    print("Loaded cogs/sftp_reader.py")
 
 @bot.event
 async def on_ready():
     print(f"Bot connected as {bot.user}")
-    # await load_cogs()
-    # await bot.tree.sync() # this is for slash-commands. Atm we're using prefixes
-# bot.run(TOKEN)
+    print(f"Started at: {time.strftime('%H:%M:%S')}")
+    # await bot.tree.sync() # this will be for slash-commands. Atm we're using prefixes
 
 # Note: Make sure to load the "cogs" files before starting the bot up
 async def main():
